6a0b7f3ac9e2eefc5761922b79765f3407cd098b
LittlePan241 <2925754909@qq.com>
2024-11-05 22:32:09 +0800

    2024/11/5

--- VSCode/Practice/Try/Try.c
+++ VSCode/Practice/Try/Try.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello World!");
-    return 0;
+    double s = 1;
+    double a = 1;
+    double b = 1;
+    for (int i = 1, j = 3; i < 11; i++, j += 2)
+    {
+        a *= i;
+        b *= j;
+        s += (a / b);
+    }
+    printf("%f", s);
 }
